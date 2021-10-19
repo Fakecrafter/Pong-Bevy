2561060084ed4e2bda8d9dc9a3baf8df30be04ba
Fakecrafter <Fakecrafter@protonmail.com>
2021-10-19 20:54:19 +0200

    ballmovement

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use bevy::prelude::*;
 
 // TODO:
 // - window descriptor
-// - ball movement
 // - ball collision with walls
 // - ball collision with paddles
 // - implement game_state
@@ -20,12 +19,15 @@ enum PaddleSize {
     Left,
 }
 
+struct BallDirection(i32, i32);
+
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .add_startup_system(setup.system())
         .add_system(paddle_movement.system())
+        .add_system(ballmovement.system())
         .run();
 }
 
@@ -67,6 +69,7 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
             ..Default::default()
         })
         .insert(Ball)
+        .insert(BallDirection(1, 1))
         .id();
 }
 
@@ -115,4 +118,10 @@ fn paddle_movement(
     }
 }
 
-fn ballmovement() {}
+fn ballmovement(time: Res<Time>, mut query: Query<(&mut Transform, &Ball, &BallDirection)>) {
+    for (mut transform, ball, direction) in query.iter_mut() {
+        let translation = &mut transform.translation;
+        translation.y += 200.0 * time.delta_seconds() * direction.0 as f32;
+        translation.x += 200.0 * time.delta_seconds() * direction.1 as f32;
+    }
+}
